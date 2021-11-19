9a8bfae2b08a8b17493bf5d29f863a15894ee8d9
Chathura <chathu.eac@gmail.com>
2021-11-19 21:30:39 +0530

    filter method added

---
+++ main.js
@@ -0,0 +1,11 @@
+// const students = [
+//     {name: 'chathura', age: 25},
+//     {name: 'thisara', age: 16},
+//     {name: 'thakshila', age: 31},
+//     {name: 'kasun', age: 12},
+//     {name: 'saman', age: 10},
+// ];
+//
+// const filteredStudents = students.filter(std => std.age < 20)
+//
+// console.log(filteredStudents)
